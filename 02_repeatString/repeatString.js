e1898c88d6fbee8377b43fd7fd4418e60c0e7fff
val-guedez <valentina.guedez.mercado@gmail.com>
2025-04-12 18:58:12 +1000

    Finish exercise02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 0) return 'ERROR';
+    let result = '';
+    for (let i = 0; i < count; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
