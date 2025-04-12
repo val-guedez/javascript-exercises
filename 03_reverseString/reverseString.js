52346fcba45c1df293f212680582a3008c119a26
val-guedez <valentina.guedez.mercado@gmail.com>
2025-04-12 19:07:11 +1000

    Finish exercise03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    if (inputString === undefined || inputString === null) throw new Error("Invalid string");
 
+    if (inputString.length === 0) return inputString;
+
+    const temp = [];
+    let reversedString = '';
+
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        reversedString += inputString[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
