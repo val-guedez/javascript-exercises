023537d51b4f290454cfce306749328b2a0b9de9
val-guedez <valentina.guedez.mercado@gmail.com>
2025-04-12 21:52:27 +1000

    Finish exercise05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return 'ERROR';
+    }
 
+    if (num1 % 1 !== 0 || num2 % 1 !== 0) {
+        return 'ERROR';
+    }
+
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    if (num1 === num2) return num1;
+
+    const min = num1 < num2 ? num1 : num2;
+    const max = num2 > num1 ? num2 : num1;
+
+    result = 0;
+
+    for (let i = min; i <= max; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
