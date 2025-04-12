341216754d4c5e7d94cbdab91a69eb2c3c002e7a
val-guedez <valentina.guedez.mercado@gmail.com>
2025-04-12 22:08:44 +1000

    Finish exercise07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFarenheit) {
+  let tempCelsius = (tempFarenheit - 32) * (5 / 9);
+  tempCelsius = Math.round(tempCelsius * 10) / 10;
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFarenheit = (tempCelsius * (9 / 5)) + 32;
+  tempFarenheit = Math.round(tempFarenheit * 10) / 10;
+  return tempFarenheit;
 };
 
 // Do not edit below this line
