b51aff8470937749ced9f4534b6d8e602a8499a6
val-guedez <valentina.guedez.mercado@gmail.com>
2025-04-13 11:05:27 +1000

    Finish basic dom manipulation practice

---
+++ DOM_Manipulation/script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "chicken jockey...";
+
+container.appendChild(content);
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "Hey I'm red :)";
+paragraph.style.color = "red";
+container.appendChild(paragraph);
+
+const heading = document.createElement("h3");
+heading.textContent = "I'm a blue h3!!";
+heading.style.color = "blue";
+container.appendChild(heading);
+
+const dressToImpress = document.createElement("div");
+dressToImpress.style.borderStyle = "solid";
+dressToImpress.style.borderColor = "black";
+dressToImpress.style.backgroundColor = "pink";
+
+
+const innerHeading = document.createElement("h1");
+innerHeading.innerText = "I'm inside a div?";
+dressToImpress.appendChild(innerHeading);
+
+const innerParagraph = document.createElement("p");
+innerParagraph.innerText = "ME TOO!";
+dressToImpress.appendChild(innerParagraph);
+
+container.appendChild(dressToImpress);
